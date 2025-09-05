9daddf8c9b384975d99bd9ca78d33af02577e0e1
GIODIROSA <giodirosaartes@gmail.com>
2025-09-05 13:39:48 -0400

    [ADD] initial project app_inacap todolist

--- bin/proyecto_uno.dart
+++
@@ -1,5 +0,0 @@
-import 'package:proyecto_uno/proyecto_uno.dart' as proyecto_uno;
-
-void main(List<String> arguments) {
-  print('Hello world: ${proyecto_uno.calculate()}!');
-}
--- test/proyecto_uno_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:proyecto_uno/proyecto_uno.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
